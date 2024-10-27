032fc2428a2a26ff43752d05c35fdcd7bf4dc76c
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-10-27 15:41:23 +0530

    pick

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
 
     //1. pick api
-    // interface User {
-    //     id: number;
-    //     name: string;
-    //     email: string;
-    //     createdAt: Date;
-    //   }
+    interface User {
+        id: number;
+        name: string;
+        email: string;
+        createdAt: Date;
+      }
       
-    //   // For a profile display, only pick `name` and `email`
-    //   type UserProfile = Pick<User, 'name' | 'email'>;
+      // For a profile display, only pick `name` and `email`
+      type UserProfile = Pick<User, 'name' | 'email'>;
       
-    //   const displayUserProfile = (user: UserProfile) => {
-    //     console.log(`Name: ${user.name}, Email: ${user.email}`);
-    //   };
+      const displayUserProfile = (user: UserProfile) => {
+        console.log(`Name: ${user.name}, Email: ${user.email}`);
+      };
       
 // 2. partial 
-interface User {
-    id: string;
-    name: string;
-    age: string;
-    email: string;
-    password: string;
-};
+// interface User {
+//     id: string;
+//     name: string;
+//     age: string;
+//     email: string;
+//     password: string;
+// };
 
-type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
+// type UpdateProps = Pick<User, 'name' | 'age' | 'email'>
 
-type UpdatePropsOptional = Partial<UpdateProps> // parital 
+// type UpdatePropsOptional = Partial<UpdateProps> // parital 
 
-function updateUser(updatedProps: UpdatePropsOptional) {
-    // hit the database tp update the user
-}
-updateUser({})
+// function updateUser(updatedProps: UpdatePropsOptional) {
+//     // hit the database tp update the user
+// }
+// updateUser({})
